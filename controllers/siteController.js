9ef989f2fa57d237cb3db9481091cb5c2acba111
Yan <127317446+yanlt32@users.noreply.github.com>
2026-07-20 22:42:38 -0300

    a

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -4,12 +4,18 @@ const Barber = require('../models/Barber');
 const GalleryPhoto = require('../models/GalleryPhoto');
 
 function index(req, res) {
+  const galleryPhotos = GalleryPhoto.all();
+  // First non-video gallery upload becomes the hero backdrop, so the
+  // homepage picks up real shop photos automatically once he uploads any.
+  const heroPhoto = galleryPhotos.find((p) => !/\.(mp4|webm|mov|m4v)$/i.test(p.path));
+
   res.render('site/index', {
     title: Setting.get('shop_name', 'Barbearia'),
     settings: Setting.getAll(),
     services: Service.all({ onlyActive: true }),
     barbers: Barber.all({ onlyActive: true }),
-    galleryPhotos: GalleryPhoto.all(),
+    galleryPhotos,
+    heroPhoto,
   });
 }
 
